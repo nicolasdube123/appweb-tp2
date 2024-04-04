1ed6ed143372f84c88e918ae8cc4a54a848d367e
nicolasdube123 <2242110@csfoy.ca>
2024-04-04 17:47:29 -0400

    Header - manque inline

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
     import Home from "./components/Home.vue"
+    import Header from "./components/Header.vue"
 </script>
 
 <template>
     <div class="container">
+        <Header/>
         <Home/>
     </div>
 </template>
