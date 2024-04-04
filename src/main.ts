6a89ce345eaed899359326f624e8a13956b31a25
Raphaël Boudreault <raphael.boudreault@gmail.com>
2024-04-04 16:41:12 -0400

    Ajout des dépendances (bootstrap, axios, etc)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "bootstrap-icons/font/bootstrap-icons.css"
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
