912a2b1a4ff5ef2eb3e64ae49265f21938e8ac63
Raphaël Boudreault <raphael.boudreault@gmail.com>
2024-04-09 14:30:54 -0400

    Triage du classement

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
-    import ShipsWithLife from "../script/shipService";
-    import { Character, CharacterService } from "../script/characterService"
+    import { Ranking, RankingService } from "../script/rankingService"
 
-    const service: CharacterService = new CharacterService();
-    let characters: Array<Character> = await service.getCharacters();
+    const service: RankingService = new RankingService();
+
+    let ranking: Array<Ranking> = await service.getRankings();
+    //Pour comparer par le score
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    ranking.sort((a, b) => { 
+        const scoreA = a.score
+        const scoreB = b.score
+        if (scoreA < scoreB) {
+            return 1
+        }
+        if (scoreA > scoreB) {
+            return -1
+        }
+        return 0
+    })
 </script>
 
 <template>
-    <div>
-        <ul v-for="character in characters">
-            <li>{{ character.name }}</li>
-        </ul>
+    <div v-for="player in ranking">
+        <p>{{ player.name }} - {{ player.score }} CG</p>
     </div>
 </template>
 
