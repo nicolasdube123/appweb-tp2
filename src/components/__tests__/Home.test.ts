8b26400d28c20557d310630dda2e9840604a2f99
Raphaël Boudreault <raphael.boudreault@gmail.com>
2024-04-25 15:43:19 -0400

    Tentative de faire fonctionner les tests

--- src/components/__tests__/Home.test.ts
+++ src/components/__tests__/Home.test.ts
@@ -1,25 +1,19 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Home from '../Home.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import router from "../../router"
 
 // TODO() : Mettre un component qui fait juste l'affichage
 
 describe('Home.vue', () => {
-  let wrapper
-
-  beforeEach(async () => {
-    await router.isReady()
-    wrapper = mount(Home, {
-      global: {
-        plugins: [router]
-      }
-    })
+  it("Test", async () => {
+    let wrapper = mount(Home)
+    const name = wrapper.find("input")
+    
+    expect(name.exists()).toBe(true)
   })
 
-  it('Lorsque le formulaire est envoyé, si le formulaire est valide, emit les informations du formulaire', async () => {
-    const name = wrapper.find("input")
+  it("Lors du lancement de l'application", async () => {
+    const name = wrapper.find("#name")
     console.log(router)
     
     expect(name.exists()).toBe(true)
