dbbc5009273cd6baf14e6482f78ed2506473f131
Raphaël Boudreault <raphael.boudreault@gmail.com>
2024-04-28 18:02:40 -0400

    Complétion des tests du component Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -42,17 +42,18 @@
             :optionToCancel=true
             :title="'Êtes-vous sûrs?'"
             :text="'Cette action va terminer la partie. Voulez-vous procéder?'"
+            id="leaving"
         />
         <div class="d-flex flex-row">
             <template v-if="props.gameStatus.hasStarted">
-                <RouterLink to="/game" class="text-decoration-none h2 me-3">Titre du jeu</RouterLink>
-                <span role="button" @click="showPopup('/')" class="text-decoration-none h3 mx-3 mt-1 text-secondary border-none">Accueil</span>
-                <span role="button" @click="showPopup('/ranking')" class="text-decoration-none h3 mx-3 mt-1 text-secondary">Score</span>
+                <RouterLink to="/game" class="text-decoration-none h2 me-3" id="title">Titre du jeu</RouterLink>
+                <span role="button" @click="showPopup('/')" class="text-decoration-none h3 mx-3 mt-1 text-secondary border-none" id="home">Accueil</span>
+                <span role="button" @click="showPopup('/ranking')" class="text-decoration-none h3 mx-3 mt-1 text-secondary" id="score">Score</span>
             </template>
             <template v-else>
-                <span class="text-decoration-none h2 me-3">Titre du jeu</span>
-                <RouterLink to="/" class="text-decoration-none h3 mx-3 mt-1 text-secondary">Accueil</RouterLink>
-                <RouterLink to="/ranking" class="text-decoration-none h3 mx-3 mt-1 text-secondary">Score</RouterLink>
+                <span class="text-decoration-none h2 me-3" id="title">Titre du jeu</span>
+                <RouterLink to="/" class="text-decoration-none h3 mx-3 mt-1 text-secondary" id="home">Accueil</RouterLink>
+                <RouterLink to="/ranking" class="text-decoration-none h3 mx-3 mt-1 text-secondary" id="score">Score</RouterLink>
             </template>
         </div>
 </template>
--- src/components/__tests__/Header.test.ts
+++ src/components/__tests__/Header.test.ts
@@ -1,14 +1,64 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Header from '../Header.vue'
+import { ref } from 'vue'
+import { GameStatus } from '../../App.vue'
 
+let gameStatusStarted: GameStatus = {
+  mission: 1,
+  hasStarted: true,
+  hasWon: undefined
+}
+let gameStatusNotStarted: GameStatus = {
+  mission: 1,
+  hasStarted: false,
+  hasWon: undefined
+}
 
 describe('Header.vue', () => {
-  // TODO()
-  it("Test", async () => {
-    //let wrapper = mount(Header)
-    //const name = wrapper.find("input")
+  it("Les champs existent en dehors de la partie", async () => {
+    let wrapper = mount(Header, {
+      propsData: {
+        gameStatus: gameStatusNotStarted
+      }
+    })
+
+    const title = wrapper.find("#title")
+    const home = wrapper.find("#home")
+    const score = wrapper.find("#score")
     
-    expect(true).toBe(true)
+    expect(title.exists()).toBeTruthy()
+    expect(home.exists()).toBeTruthy()
+    expect(score.exists()).toBeTruthy()
+  })
+
+  it("Les champs existent dans la partie", async () => {
+    let wrapper = mount(Header, {
+      propsData: {
+        gameStatus: gameStatusStarted
+      }
+    })
+
+    const title = wrapper.find("#title")
+    const home = wrapper.find("#home")
+    const score = wrapper.find("#score")
+    
+    expect(title.exists()).toBeTruthy()
+    expect(home.exists()).toBeTruthy()
+    expect(score.exists()).toBeTruthy()
+  })
+
+  it("Si le joueur veut quitter la partie, un popup de confirmation apparait", async () => {
+    let wrapper = mount(Header, {
+      propsData: {
+        gameStatus: gameStatusStarted
+      }
+    })
+
+    wrapper.find("#home").trigger("click")
+    await wrapper.vm.$nextTick()
+    const popup = wrapper.find("#leaving")
+
+    expect(popup.exists()).toBeTruthy()
   })
 })
